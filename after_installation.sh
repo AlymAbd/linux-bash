92f67a655f9d5eca0187805e5452c868dd3c06f0
AlymAbd <45037307+AlymAbd@users.noreply.github.com>
2021-11-07 16:04:54 +0600

    Create after_installation.sh

---
+++ after_installation.sh
@@ -0,0 +1,12 @@
+sudo dnf update -y
+
+
+# DOCKER
+sudo dnf install 
+
+
+# Python Connection
+sudo dnf install python3-devel
+sudo dnf install mysql mysql-connector-python3 mysql++-devel -y
+
+sudo dnf install postgresql postgresql-devel
